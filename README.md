a1c8eb713b0981b64b8f77ea3db767129e8f0c79
freeall <freeall@gmail.com>
2013-06-15 12:56:54 +0200

    init

--- README.md
+++ README.md
@@ -1,4 +1,142 @@
-tvcom
-=====
+# tvcom
 
-A simple module for getting data from tv.com
+A simple way to get data from the tv.com website.
+
+## Usage
+
+tvcom is pretty straight forward. It has three methods, `shows`, `episodes`, `summary`.
+
+
+### shows(number, callback)
+
+Returns an array of the `number` most popular tv-shows. It's should noted that this list is not sorted
+
+``` js
+var tvcom = require('tvcom');
+tvcom.shows(10, function(err, shows) { ... });
+
+/*
+{
+	'game-of-thrones': {
+		id: 'game-of-thrones',
+		title: 'Game of Thrones',
+		icon: 'http://im0n.clkimg.com/i/sm/069/69088.jpg',
+		description: 'Game of Thrones is an ...'
+	},
+ 	'greys-anatomy': {
+ 		id: 'greys-anatomy',
+		title: 'Grey\'s Anatomy',
+		icon: 'http://im1n.clkimg.com/i/sm/000/227.jpg',
+		description: 'Grey\'s Anatomy is a ...'
+	},
+	...
+}
+*/
+```
+
+### episodes(id, callback)
+
+Returns an array with all the seasons, with an array of all the episodes in that season.
+
+``` js
+var tvcom = require('tvcom');
+tvcom.episodes('game-of-thrones', function(err, episodes) { ... });
+
+/*
+[
+	[
+		{
+			title: 'A Hard Day\'s Night',
+			firstAired: '3/27/05',
+			season: '1',
+			episode: '1',
+			rating: '9.3',
+			description: 'Meet Meredith Grey. Daughter of ...'
+		},
+		{
+			title: 'The First Cut Is the Deepest',
+			firstAired: '4/3/05',
+			season: '1',
+			episode: '2',
+			rating: '9.1',
+			description: 'The surgical staff tries to ...'
+		},
+		...
+	],
+	[
+		{
+			title: 'Raindrops Keep Falling On My Head',
+			firstAired: '9/25/05',
+			season: '2',
+			episode: '1',
+			rating: '9.3',
+			description: 'Picks up where ...'
+		},
+		...
+	],
+	...
+]
+*/
+```
+
+### summary(id, callback)
+
+Returns an object with a description, some images, videos, and news. The data is from http://www.tv.com/shows/greys-anatomy
+
+``` js
+var tvcom = require('tvcom');
+tvcom.summary('game-of-thrones', function(err, summary) { ... });
+
+/*
+{
+	title: 'Grey\'s Anatomy',
+	rating: '8.8',
+	description: 'Grey\'s Anatomy is a ...' }
+	cast: [
+		{
+			name: 'Camilla Luddington',
+			role: 'Dr. Jo Wilson (Season 10+)',
+			icon: '//dw.com.com/clear/c.gif?sid=1003&ptid=6138'
+		},
+		...
+	],
+	news: [
+		{
+			teaser: 'TV IN DA CLUBUPFRONTS 2013',
+			title: 'News Briefs: Starz and 50 Cent Are Powering Up for a New DramaABC\'s 2013-2014 Schedule: S.H.I.E.L.D. on Tuesdays, Less DWTS, and OUAT \'s New Spinoff on Thursdays',
+			link: 'http://tv.com/news/news-briefs-starz-and-50-cent-are-powering-up-for-a-new-drama-137124191630/',
+			icon: '//dw.com.com/clear/c.gif?sid=1003&ptid=6138',
+			comments: '7',
+			date: '10 hours ago',
+		    by: {
+		    	name: 'Tim Surette',
+				link: 'http://tv.com/news/author/TimSpot/'
+			}
+		},
+		...
+	],
+	photos: [
+		{
+			icon: 'http://static.tvtome.com/images/misc/grey.gif',
+			link: 'http://tv.com/shows/greys-anatomy/photos/image-1684/',
+			title: 'Patrick Dempsey and Ellen Pompeo on Grey...',
+			credit: '© 2013 American Broadcasting Companies, Inc.'
+		},
+		...
+	],
+	clips: [
+		{
+			icon: 'http://im0n.clkimg.com/i/em/2/325/2325054.jpg',
+			link: 'http://tv.com/shows/greys-anatomy/watch/jessica-capshaw-interview-1882579/',
+			title: 'Jessica Capshaw Interview',
+			date: '9/25/09',
+			time: '02:58'
+		},
+		...
+	]
+*/
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,145 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+request = request.defaults({
+	jar: false
+});
+
+var onresponse = function(onerror, callback) {
+	return function(err, response) {
+		if (err) return onerror(err);
+		callback(cheerio.load(response.body), response);
+	};
+};
+var text = function($elem) {
+	return $elem.text().trim().replace('\r\n', '\n').replace(/moreless$/, '');
+};
+var shows = function(count, callback) {
+	var res = {};
+
+	var fetchPage = function(page, onpage) {
+		request.get('http://tv.com/shows/page'+page + '/', onresponse(callback, function($, response) {
+			console.log(1)
+			$('._bento .show').each(function(_, show) {
+				if (Object.keys(res).length >= count) return;
+
+				var $show = $(show);
+				var id = $('.info h4 a', $show).attr('href').match(/shows\/(.*)\//)[1];
+				res[id] = {
+					id: id,
+					title: text($('.info h4 a', $show)),
+					icon:  $('._image_container img', $show).attr('src'),
+					description: text($('.info ._more_less', $show)),
+				};
+			});
+
+			if (Object.keys(res).length >= count) return callback(null, res);
+
+			onpage();
+		}));
+	};
+
+	var i=1;
+	function next() {
+		fetchPage(i++, next);
+	}
+	next();
+};
+var episodes = function(id, callback) {
+	var res = [];
+
+	request.get('http://tv.com/shows/'+id+'/episodes/?printable=1', onresponse(callback, function($, response) {
+		var formerSeason = '';
+		$('.episodes .episode').toArray().reverse().forEach(function(episode) {
+			var $episode = $(episode);
+			var $dds = $('.info dd', $episode);
+			var season = text($dds.eq(1));
+			var episode = text($dds.eq(2));
+			var thisEpisode = {
+				title: text($('.title', $episode)),
+				firstAired: text($dds.eq(0)),
+				season: season,
+				episode: episode,
+				rating: $dds.eq(3).text().match(/(\d\.\d)/)[1],
+				description: text($('.description', $episode))
+			};
+
+			if (season !== formerSeason) {
+				formerSeason = season;
+				res.push([]);
+			}
+			var s = res[res.length-1];
+			s.push(thisEpisode);
+		});
+
+		callback(null, res);
+	}));
+};
+var summary = function(id, callback) {
+	var res = {
+		cast: [],
+		news: [],
+		photos: [],
+		clips: []
+	};
+
+	request.get('http://tv.com/shows/'+id, onresponse(callback, function($, response) {
+		res.title = text($('h1[data-name]'));
+		res.rating = text($('.global .score'));
+		res.description = text($('.description'));
+
+		$('.persons .person').each(function(_, person) {
+			var $person = $(person);
+			res.cast.push({
+				name: text($('.name', $person)),
+				role: text($('.role', $person)),
+				icon: $('img').attr('src')
+			});
+		});
+		$('.latest_news ._story_capsule').each(function(_, news) {
+			var $news = $(news);
+			res.news.push({
+				teaser: text($('.info h5'), $news),
+				title: text($('.info .title'), $news),
+				link: 'http://tv.com' + $('.info .title a').attr('href'),
+				icon: $('img').attr('src'),
+				comments: text($('.comment_count', $news)),
+				date: $('.byline', $news)[0].children[2].data.trim().replace(/^,\s/,''),
+				by: {
+					name: text($('.byline span', $news)),
+					link: 'http://tv.com' + $('.byline a', $news).attr('href')
+				}
+			});
+		});
+		$('.photos .photo').each(function(_, photo) {
+			var $photo = $(photo);
+			res.photos.push({
+				icon: $('img', $photo).attr('src'),
+				link: 'http://tv.com'+$('.caption a', $photo).attr('href'),
+				title: text($('.caption a', $photo)),
+				credit: text($('.credit', $photo))
+			});
+		});
+		$('.vid', $('.clips').last()).each(function(_, vid) {
+			var $vid = $(vid);
+			var dt = text($('.date_time', $vid)).match(/(.+\/.+\/.+)*\s*\((.*\:.+)\)/m);
+			res.clips.push({
+				icon: $('img', $vid).attr('src'),
+				link: 'http://tv.com'+$('.mask', $vid).attr('href'),
+				title: text($('.title', $vid)),
+				date: dt[1],
+				time: dt[2]
+			});
+		});
+
+		callback(null, res);
+	}));
+};
+
+module.exports = {
+	shows: shows,
+	episodes: episodes,
+	summary: summary
+};
+
+summary('greys-anatomy', function(err, res) { console.log(res)})
